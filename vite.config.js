8edf9fab73b4373b1204724a0372ea54efd4d310
Kim-buyeon <162018256+Kim-buyeon@users.noreply.github.com>
2025-12-01 03:07:42 +0900

    Revert "Feature/final version UI  branch에서 ocr api 부분 제외하고 통합하여 1차 merge 진행"

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
-import { screenGraphPlugin } from "@animaapp/vite-plugin-screen-graph";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === "development" && screenGraphPlugin()],
-  publicDir: "./static",
-  base: "./",
-}));
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+})
